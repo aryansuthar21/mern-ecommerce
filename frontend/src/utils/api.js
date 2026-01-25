afba8518295a49bec85d9c0d7ee8865b19efe590
Aryan Suthar <aryansuthar3670@email.com>
2026-01-25 15:57:14 +0530

    Fix CORS and connect frontend to backend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL:"https://mern-ecommerce-backend-4cby.onrender.com",
 })
 
 export default api
